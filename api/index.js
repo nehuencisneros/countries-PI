89751c97e3760b7083c02e47a1e1c6f0eb10d6f3
Nehuen Cisneros <cisneros.n.b@outlook.com>
2023-07-13 22:09:14 -0300

    ultimos cambios

--- api/index.js
+++ api/index.js
@@ -26,6 +26,6 @@ const { conn } = require('./src/db.js');
 // Syncing all the models at once.
 conn.sync({ alter: true }).then(() => {
   server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log('ready at 3001'); // eslint-disable-line no-console
   });
 });
